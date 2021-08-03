b9653548d7c4f94f07f95333fc49b5f162d3e0fe
gr2nsky <dbswovlf2009@naver.com>
2021-08-03 12:46:31 +0900

    addBtn의 action으로 액션시트 생성

--- Standard/StandardImagePicker/StandardImagePicker/ViewController.swift
+++ Standard/StandardImagePicker/StandardImagePicker/ViewController.swift
@@ -16,7 +16,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func addAction(_ sender: Any) {
+    @IBAction func addAction(_ sender: UIButton) {
+        
+        let alter = UIAlertController(title: "제목", message: "메시지", preferredStyle: .actionSheet)
+        
+        let gallery = UIAlertAction(title: "사진앨범", style: .default, handler: {ACTIN in in self.openLibrary()})
+        
+        let camera = UIAlertAction(title: "카메라", style: .default, handler: ACTION in self.openCamera())
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alter.addAction(gallery)
+        alter.addAction(camera)
+        alter.addAction(cancel)
+        
+        present(alter, animated: true, completion: nil)
     }
     
 }
